07ffc30d681f8096f547970899a57bc7e5d03fbc
NautisShadrick <33418645+NautisShadrick@users.noreply.github.com>
2024-09-04 13:53:19 +0300

    Moved HelperFunctions to Utils

--- FishingWorld/Assets/Scripts/PlayerTracker.lua
+++ FishingWorld/Assets/Scripts/PlayerTracker.lua
@@ -256,52 +256,6 @@ function GetPlayerItemCount(player, itemID)
     return playerInventory[itemIndex].amount
 end
 
-function is_table_equal(t1,t2,ignore_mt)
-    local ty1 = type(t1)
-    local ty2 = type(t2)
-    if ty1 ~= ty2 then return false end
-    -- non-table types can be directly compared
-    if ty1 ~= 'table' and ty2 ~= 'table' then return t1 == t2 end
-    -- as well as tables which have the metamethod __eq
-    local mt = getmetatable(t1)
-    if not ignore_mt and mt and mt.__eq then return t1 == t2 end
-    for k1,v1 in pairs(t1) do
-       local v2 = t2[k1]
-       if v2 == nil or not is_table_equal(v1,v2) then return false end
-    end
-    for k2,v2 in pairs(t2) do
-       local v1 = t1[k2]
-       if v1 == nil or not is_table_equal(v1,v2) then return false end
-    end
-    return true
-end
-
-function SortScores()
-    -- Create a sortable list of players with score
-    local sortableScores = {}
-    for playerName, playerInfo in topScoreTable do
-        table.insert(sortableScores, {playerName = playerInfo.playerName, playerScore = playerInfo.playerScore})
-    end
-
-    -- Sort the list by score in descending order
-    table.sort(sortableScores, function(a, b)
-        return a.playerScore > b.playerScore
-    end)
-
-    -- Extract the top scores (up to the number of players available)
-    local numPlayers = #sortableScores
-    local topScoresCount = math.min(numPlayers, 10)
-
-    local topScores = {}
-    local iTopScores = {}
-    for i = 1, topScoresCount do
-        topScores[sortableScores[i].playerName] = sortableScores[i]
-        table.insert(iTopScores, sortableScores[i])
-    end
-
-    return topScores, iTopScores;
-end
-
 function GetTokens(player)
     return players[player].Tokens.value
 end
@@ -321,11 +275,11 @@ function AddScoreServer(player, amount)
             playerScore = value
         }
         
-        local topScores, iTopScores = SortScores()
+        local topScores, iTopScores = utils.SortScores(topScoreTable)
 
         local leaderboardChanged = false
         Storage.UpdateValue("TopScores", function(newScores)
-            leaderboardChanged = newScores == nil or not is_table_equal(newScores, topScores, false)
+            leaderboardChanged = newScores == nil or not utils.is_table_equal(newScores, topScores, false)
             return if leaderboardChanged then topScores else nil
         end,
         function()
@@ -347,7 +301,7 @@ function UpdatePlayerScore(player)
 end
 
 function UpdateLeaderboard()
-    local topScores, iTopScores = SortScores()
+    local topScores, iTopScores = utils.SortScores(topScoreTable)
     ServerLeaderboard.value = iTopScores
 end
 
