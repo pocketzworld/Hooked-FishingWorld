1b95345a46cee67e92ce40b0e72a81e76ee211b2
NautisShadrick <33418645+NautisShadrick@users.noreply.github.com>
2024-09-18 13:32:51 +0300

    UI Polishing
    1. Added Map button to World HUD
    2. Added tooltips to player stats
    4. Few changes in player stats

--- FishingWorld/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/worldhud.cs
+++ FishingWorld/Packages/com.pz.studio.generated/Runtime/Highrise.Lua.Generated/worldhud.cs
@@ -51,6 +51,7 @@ namespace Highrise.Lua.Generated
                 CreateSerializedProperty(_script.GetPropertyAt(13), null),
                 CreateSerializedProperty(_script.GetPropertyAt(14), null),
                 CreateSerializedProperty(_script.GetPropertyAt(15), null),
+                CreateSerializedProperty(_script.GetPropertyAt(16), null),
             };
         }
     }
